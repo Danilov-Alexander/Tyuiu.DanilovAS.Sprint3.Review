9afccdea5e579a25d08aca1cd72b695e7e60ba06
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-11-18 07:20:48 +0500

    Add project files.

---
+++ Tyuiu.DanilovAS.Sprint3.TaskReview.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DanilovAS.Sprint3.TaskReview.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DanilovAS.Sprint3.TaskReview.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b02bf6a-6e17-439c-8c63-ba2b61cc68e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DanilovAS.Sprint3.TaskReview.V22/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DanilovAS.Sprint3.TaskReview.V22.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint3.TaskReview.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт(Review)#3 | Выполнил: Данилов А. С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт(Review)#3                                                        *");
+            Console.WriteLine("* Тема: Задание по спринту                       				         *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИСТНб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+            Console.WriteLine("* F(x) = sin(x) + (cos(2*x))/(2) - 1.5*x (произвести табулирование) f(x)  *");
+            Console.WriteLine("* на заданном диапазоне [-5;5] с шагом 1. Произвести проверку деления на  *");
+            Console.WriteLine("* ноль вернуть значение 0.*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = {0}", startValue);
+            Console.WriteLine("Конец шага = {0}", stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("+---------+---------+");
+            Console.WriteLine("|    X    |   f(x)  |");
+            Console.WriteLine("+---------+---------+");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}    |  {1, 5:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+
+            Console.WriteLine("+---------+---------+");
+            Console.ReadKey();
+        }
+    }
+}
